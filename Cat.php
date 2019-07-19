0f4f54bbf3680c931f30e822ddd9b9f7f70b7c12
Дарья <dariakurbasova@gmail.com>
2019-07-19 20:00:17 +0300

    Проверка на 3 действия подряд - отдельный метод

--- Cat.php
+++ Cat.php
@@ -12,6 +12,7 @@ class Cat
     CONST COMMUNICATE_MESSAGE_LIKE = "Люблю тебя, человек!";
     CONST COMMUNICATE_MESSAGE_HATE = "Отстань от меня!";
     CONST TIRED_OF_IT_MESSAGE = "Может, попробуешь что-нибудь другое?";
+    CONST STOP_MESSAGE = "Хватит баловать котика, больше нельзя";
 
 
     /** @var Game */
@@ -26,16 +27,25 @@ class Cat
         $this->name = $name;
     }
 
+    // Одни и те же действия быстро наскучат котику. Нужно не больше 3 одинаковых подряд.
+    public function check_same_actions ($action) {
+        $history = $this->game->action_history;
+        if ($history[count($history)-1] == $history[count($history)-2] && $history[count($history)-1] == $history[count($history)-3]) {
+            $this->$action += 10;
+            $this->mood -= 20;
+            $this->game->message = self::TIRED_OF_IT_MESSAGE;
+            return null;
+        } else {
+            return 5;
+        }
+    }
+
     // Покормить котика одним из 3 видов корма: сухой, влажный и домашний. Котан привередливый, не все корма любит одинково.
     public function feed_dry()
     {
-        $history = $this->game->action_history;
         $history[] = "feed_dry";
-        if ($history[count($history)-2]=="feed_dry" && $history[count($history)-3]=="feed_dry") {
-            $this->food += 10;
-            $this->mood -= 20;
-            $this->game->message = self::TIRED_OF_IT_MESSAGE;
-        } else {
+        $this->check_same_actions("food");
+        if ($this->check_same_actions("food") == 5) {
             $probability_like = rand(1, 10);
             if ($probability_like == 10) {
                 $this->mood -= 10;
@@ -48,13 +58,9 @@ class Cat
         }
     }
     public function feed_wet () {
-        $history = $this->game->action_history;
         $history[] = "feed_wet";
-        if ($history[count($history)-2]=="feed_dry" && $history[count($history)-3]=="feed_dry") {
-            $this->food += 10;
-            $this->mood -= 20;
-            $this->game->message = self::TIRED_OF_IT_MESSAGE;
-        } else {
+        $this->check_same_actions("food");
+        if ($this->check_same_actions("food") == 5) {
             $probability_like = rand(1, 10);
             if ($probability_like > 5) {
                 $this->mood -= 10;
@@ -67,28 +73,24 @@ class Cat
         }
     }
     public function feed_home () {
-        $history = $this->game->action_history;
         $history[] = "feed_home";
-        if ($history[count($history)-2]=="feed_dry" && $history[count($history)-3]=="feed_dry") {
-            $this->food += 10;
-            $this->mood -= 20;
-            $this->game->message = self::TIRED_OF_IT_MESSAGE;
+        $this->check_same_actions("food");
+        if (array_count_values($history)["feed-home"] < 3) {
+            if ($this->check_same_actions("food") == 5) {
+                $this->mood += 15;
+                $this->game->message = self::EAT_MESSAGE_LIKE;
+                $this->food += 10;
+            }
         } else {
-            $this->mood += 15;
-            $this->game->message = self::EAT_MESSAGE_LIKE;
-            $this->food += 10;
+            $this->game->message = self::STOP_MESSAGE;
         }
     }
 
     // Погладить
     public function stroke () {
-        $history = $this->game->action_history;
         $history[] = "stroke";
-        if ($history[count($history)-2]=="feed_dry" && $history[count($history)-3]=="feed_dry") {
-            $this->communication += 10;
-            $this->mood -= 20;
-            $this->game->message = self::TIRED_OF_IT_MESSAGE;
-        } else {
+        $this->check_same_actions("communication");
+        if ($this->check_same_actions("communication") == 5) {
             $probability_like = rand(1, 10);
             if ($probability_like > 5) {
                 $this->mood -= 10;
@@ -102,13 +104,9 @@ class Cat
     }
     // Поиграть с дразнилкой
     public function play_teaser () {
-        $history = $this->game->action_history;
         $history[] = "play_teaser";
-        if ($history[count($history)-2]=="feed_dry" && $history[count($history)-3]=="feed_dry") {
-            $this->communication += 10;
-            $this->mood -= 20;
-            $this->game->message = self::TIRED_OF_IT_MESSAGE;
-        } else {
+        $this->check_same_actions("communication");
+        if ($this->check_same_actions("communication") == 5) {
             $probability_like = rand(1, 10);
             if ($probability_like > 8) {
                 $this->mood -= 10;
@@ -122,13 +120,9 @@ class Cat
     }
     // Поиграть с мышкой
     public function play_mouse () {
-        $history = $this->game->action_history;
         $history[] = "play_teaser";
-        if ($history[count($history)-2]=="feed_dry" && $history[count($history)-3]=="feed_dry") {
-            $this->communication += 10;
-            $this->mood -= 20;
-            $this->game->message = self::TIRED_OF_IT_MESSAGE;
-        } else {
+        $this->check_same_actions("communication");
+        if ($this->check_same_actions("communication") == 5) {
             $probability_like = rand(1, 10);
             if ($probability_like > 6) {
                 $this->mood -= 10;
--- index.php
+++ index.php
@@ -8,6 +8,9 @@ if (!empty($_SESSION['game'])) {
     var_dump($game);
 
     if (!empty($game->message)) {
+        echo "<br>" . $game->cat->food;
+        echo "<br>" . $game->cat->communication;
+        echo "<br>" . $game->cat->mood;
         echo "<br>" . $game->message;
     }
 
